76445103bd99da9032404862faea26a746d8bd42
sonhoangnhatvi <84952026+sonhoangnhatvi@users.noreply.github.com>
2024-05-20 13:41:26 +0700

    Add Config, Fetch Data From Firebase in MainNavigation
    Add Config, Fetch Data From Firebase in MainNavigation

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -1,7 +1,7 @@
 export const ArticlePage = () => {
   return (
     <div>
-      <h1>Article</h1>
+      <h1>Article Page</h1>
     </div>
   );
 };
--- src/pages/DocumentPage/DocumentPage.tsx
+++ src/pages/DocumentPage/DocumentPage.tsx
@@ -1,7 +1,7 @@
 export const DocumentPage = () => {
   return (
     <div>
-      <h1>Document</h1>
+      <h1>Document Page</h1>
     </div>
   );
 };
